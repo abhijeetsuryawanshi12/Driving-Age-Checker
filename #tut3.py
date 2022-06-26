bce81b35a2c79c8ae8075f7772a55759894987a4
abhi <abhijeetsuryawanshi128@gmail.com>
2022-06-26 20:34:37 +0530

    first commit

---
+++ #tut3.py
@@ -0,0 +1,9 @@
+age1 = 18
+age3 = 50
+age2 = int(input())
+if age1 < age2 < age3:
+    print("he can drive")
+elif age2 == age1:
+    print("he should come to physical check up")
+else:
+    print("he can not drive")
